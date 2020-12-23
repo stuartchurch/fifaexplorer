fa9fd1014c57d12211421df9192955d20bbc3f23
stuartchurch <stuart.church@gmail.com>
2020-12-23 12:48:22 +0000

    Update fifaval.py

--- fifaval.py
+++ fifaval.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 @st.cache
-url = https://github.com/stuartchurch/fifaexplorer/blob/main/All_Player_List.csv
+url = "https://github.com/stuartchurch/fifaexplorer/blob/main/All_Player_List.csv"
 df = pd.read_csv(url)
 df['delta'] = df['Potential Score'] - df['Overall Score']
 
