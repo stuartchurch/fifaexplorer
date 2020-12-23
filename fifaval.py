be5ac305f7513e2414c5e397d780874a45b645fa
stuartchurch <stuart.church@gmail.com>
2020-12-23 14:27:44 +0000

    Update fifaval.py

--- fifaval.py
+++ fifaval.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-import numpy as np
 
-#@st.cache
+@st.cache
 df=pd.read_csv('All_Player_List.csv')
 
 df['delta'] = df['Potential Score'] - df['Overall Score']
