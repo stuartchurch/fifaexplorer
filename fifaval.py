02f8cac3ecd31bf7b7556790547eb1c4c4db19ad
stuartchurch <stuart.church@gmail.com>
2020-12-23 12:53:53 +0000

    Update fifaval.py

--- fifaval.py
+++ fifaval.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 #@st.cache
-url = "https://github.com/stuartchurch/fifaexplorer/blob/main/All_Player_List.csv"
-df = pd.read_csv(url)
-df['delta'] = df['Potential Score'] - df['Overall Score']
+#url = "https://github.com/stuartchurch/fifaexplorer/blob/main/All_Player_List.csv"
+#df = pd.read_csv(url)
+#df['delta'] = df['Potential Score'] - df['Overall Score']
 
 st.title("Exploring the Market Value of FIFA 20 Players")
 
